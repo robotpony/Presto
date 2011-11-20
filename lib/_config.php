f0b75afd4615e2031524b37556ba8d7aaccbc7d3
Bruce Alderson <mx@warpedvisions.org>
2011-11-19 17:18:03 -0800

    Added documentation notes.

--- lib/_config.php
+++ lib/_config.php
@@ -4,7 +4,8 @@
 $builtIns = array('API', 'request', 'session');
 
 define('PRESTO_DEBUG', 0);
-define('ACTION_HEADER', 'X-Action');
+
+define('ACTION_HEADER', 'X-Action'); // TODO - consider dropping this concept
 define('DEFAULT_RES_TYPE', '.html');
 
 ?>
