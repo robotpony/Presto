a0d6e81e1290fddd4c43d8f6ba6fbe274167e031
Bruce Alderson <mx@warpedvisions.org>
2011-11-19 16:50:54 -0800

    Moved presto creation to delegator, and added basic error handling.

--- lib/delegator-index.php
+++ lib/delegator-index.php
@@ -1,11 +1,17 @@
 <?php include_once('./lib/Presto.php'); ?>
 
-<h1>Presto test</h1>
+<h1>Presto debug mode</h1>
 
 <?php 
 
-global $p;
+try {
+	
+	$p = new Presto();
+	dump($p);
+	
+} catch (Exception $e) {
+	print 'Presto error: ' . $e->getMessage();
+}
 
-dump($p);
 
 ?>
