38d94c7d108db1ede9ef8073c0ef3a737ae1bc3a
Bruce Alderson <mx@warpedvisions.org>
2011-11-19 17:55:25 -0800

    Added response hookup for APIs (and cleared up parameter naming).

--- lib/API.php
+++ lib/API.php
@@ -3,11 +3,13 @@
 class API {
 
 	public static $req;
+	public static $resp; 
 	public static $sess; 
 
-	public function __construct($r,$s) {	
-		self::$req = $r;
-		self::$sess = $s;
+	public function __construct($req, $sess, $resp) {	
+		self::$req = $req;
+		self::$resp = $resp;
+		self::$sess = $sess;
 	}
 	
 	public function status() {}
