ebf3590c88c41f4102512553c1c44a5f22ac1827
Bruce Alderson <mx@warpedvisions.org>
2011-11-19 17:15:20 -0800

    Added simple filtering for dispatch components, to allow dashes in root request concepts (/thing-name/parameter.json).

--- lib/Request.php
+++ lib/Request.php
@@ -23,7 +23,7 @@ class URI {
 	public function opt($k) { return (array_key_exists($k, $this->options)) 
 		? $this->options[$k] : NULL ; }
 		
-	public function component($d) { return coalesce( array_shift($this->parameters), $d ); }
+	public function component($d) { return coalesce( str_replace('-', '_', array_shift($this->parameters)), $d ); }
 }
 class Request {
 
