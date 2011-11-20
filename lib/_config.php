a02c621fa7f4554f24a6d84910dd93d16e9dd442
Bruce Alderson <mx@warpedvisions.org>
2011-11-20 11:06:44 -0800

    Removed unused constant.

--- lib/_config.php
+++ lib/_config.php
@@ -6,7 +6,6 @@ $builtIns = array('API', 'request', 'session');
 define('PRESTO_DEBUG', 0);
 define('VERSION_HEADER', 'X-Api-Version');
 define('API_VERSION', 0);
-define('ACTION_HEADER', 'X-Action'); // TODO - consider dropping this concept
 define('DEFAULT_RES_TYPE', '.html');
 
 
