fbbce9fc1c6a708d80a876d82faa5bd965edcb17
Bruce Alderson <mx@warpedvisions.org>
2011-11-19 17:08:03 -0800

    Added debug mode to delegator, to allow debugging of delegation logic.

--- lib/delegator-index.php
+++ lib/delegator-index.php
@@ -1,13 +1,16 @@
 <?php include_once('./lib/Presto.php'); ?>
 
+<?php if (PRESTO_DEBUG) { ?>
 <h1>Presto debug mode</h1>
+<?php } ?>
 
 <?php 
 
 try {
 	
 	$p = new Presto();
-	dump($p);
+	
+	if (PRESTO_DEBUG) dump($p);
 	
 } catch (Exception $e) {
 	print 'Presto error: ' . $e->getMessage();
