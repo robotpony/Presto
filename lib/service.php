ab02e59e45b0f59455dc38f1ee5cdf53dac01368
Bruce Alderson <bruce@napkinware.com>
2011-11-29 15:35:59 -0800

    Updated error output formatting (less extra whitespace).

--- lib/service.php
+++ lib/service.php
@@ -222,7 +222,7 @@ class Service {
 		if ($this->call->info->http_code != 200) {
 			$dump = ($this->options->debug) ? print_r($this->result, true) 
 				: print_r($this->result->data, true);
-			throw new Exception("HTTP error\n{$this->call->method} {$this->call->uri}\n\n$dump\n\n" , $this->call->info->http_code);
+			throw new Exception("HTTP error\n{$this->call->method} {$this->call->uri}\n\n$dump\n" , $this->call->info->http_code);
 		}
 		
 		return $this->data();
