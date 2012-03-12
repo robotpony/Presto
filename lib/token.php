b0a1ac29d08bc8b93501f197620a857409c470d7
Bruce Alderson <bruce@napkinware.com>
2012-03-12 10:36:19 -0700

    Added encrytped token class, for secure token handling.

---
+++ lib/token.php
@@ -0,0 +1,125 @@
+<?php 
+/** An authentication token 
+
+Useful as a GET parameter, header value, or cookie value. Tokens are both encryped and
+validated with a hash value, and can contain a few Kb of key/value pairs.
+
+
+Note:
+
+* You must define TOKEN_ENCRYPTION, TOKEN_SIGNING_KEY, and SIGNING_INIT before
+  working with a token
+  
+TODO:
+	* support for W rollover
+	* expose R parameters (and define structure) ... pass off to second class?
+*/
+class token {
+	private $t;
+	private $p;
+	private $hash;
+
+	/* Create a token from parts or a token string */
+	public function __construct($v) {
+	
+		if (is_array($v)) { // build from parameters			
+			$this->build($v, false /* force creation of check + timestamp */ );
+			$this->encrypt();			
+		} elseif (is_string($v)) { // build from encrypted string
+			$this->t = $v;
+			$this->decrypt();			
+		} else
+			throw new Exception('Missing token or token parts.', 500);
+	}
+	
+	/* Get the encoded token */
+	public function encoded() { $this->encrypt(); return $this->t; }
+	/* Get the token parts */
+	public function parts() {return $this->p;}
+	/* Is this token valid? */
+	public function ok() {return count($this->p) && !empty($this->p->h) 
+		&& $this->p->h == $this->hash; }
+	
+	/* Get the checked parts as a string (for calculating checks) */	
+	private function checked_parts() {
+		if (empty($this->p)) throw new Exception('Token is not initialized.', 500);
+		$elements = array($this->p->name, $this->p->email, $this->p->id, $this->p->acct, 
+						AUTH_SECRET, $this->p->t);	
+		
+		foreach ($elements as &$e) $e = urlencode($e);
+
+		return implode('', $elements);
+	}
+
+	/* Get a token as an encoded URI string */	
+	private function uri() {
+		if (empty($this->p)) throw new Exception('Token is not initialized.', 500);
+		$uri = '';
+		foreach ($this->p as $k => $v) $uri .= $k.'='.urlencode($v).'&';
+		return $uri;				
+	}
+	
+	/* Build the token object from parts */
+	private function build($p, $strict = true) {
+
+		// apply defaults
+		$this->set($p['t'], time(), $strict);
+		$this->set($p['w'], 60, $strict);
+				
+		// check for required elements
+		foreach (array('name', 'email', 'id', 'acct', 'a', 'r') as $k)
+			if (empty($p[$k])) // missing a required token element
+				throw new Exception('Invalid credentials, missing: '.$k, 401);
+
+		foreach ($p as $k => &$v)
+			$v = urldecode($v); // remove URI encoding
+				
+		$this->p = (object) $p; // objectize for convinience
+		$this->roles = json_decode($this->p->r); // pull out roles (if available)
+
+		// lastly, apply a default value to the CRC (if needed/possible)
+		$this->set($this->p->h, sha1($this->checked_parts()), $strict);		
+		$this->hash = sha1($this->checked_parts());
+				
+		return $this->hash == $this->p->h;
+	}
+	
+	/* Assign a token value a default (if possible) */ 
+	private function set(&$v, $d, $strict = true) {
+		if (empty($v)) {
+			if (!$strict) $v = $d; 
+			else throw new Exception('Missing required check field.', 401);
+		}
+	}
+	
+	/* Encrypt a token from parts */
+	private function encrypt() {	
+		$this->t = openssl_encrypt($this->uri(), 
+			TOKEN_ENCRYPTION, TOKEN_SIGNING_KEY, false, SIGNING_INIT);
+			
+		return $this->t;
+	}
+	
+	/* Decrypt a token into parts (thows on errors) */
+	private function decrypt() {		
+		$t = openssl_decrypt($this->t, 
+			TOKEN_ENCRYPTION, TOKEN_SIGNING_KEY, false, SIGNING_INIT);
+
+		if (empty($t))
+			throw new Exception('Token empty.', 401);
+
+		parse_str( $t, $p );
+
+		if (empty($p))
+			throw new Exception('Token format invalid.', 401);
+		
+		$this->build( $p );
+		$this->hash = sha1($this->checked_parts());
+			
+		if ($this->hash != $this->p->h)
+			throw new Exception('Token integrity check failed.', 401);	
+			
+		return $this->p;
+	}
+
+}
