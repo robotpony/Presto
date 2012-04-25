d3a969d57bcdbde0f34cef3da47fc8a6504793fc
Bruce Alderson <balderson@discoverysoftware.com>
2012-04-25 10:47:54 -0700

    Added the start of a simple DB wrapper to make PDO use simpler (and more like CI).

---
+++ lib/db.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Database wrapper
+
+	See PDO docs for details: http://www.php.net/manual/en/class.pdo.php
+
+	Adds a few extensions for common batched operations.
+*/
+class db extends PDO {
+
+	private $statement; 
+	
+	/* Returns an array of classes based on the given SQL and bound parameters (see PDO docs for details) */
+	function select($sql, $bound_parameters = array()) {		
+		$this->statement = $this->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$this->statement->execute($bound_parameters);
+		return $this->statement->fetchAll(PDO::FETCH_CLASS);	
+	}
+
+}
