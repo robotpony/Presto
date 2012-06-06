763693f75eebb24b2d6eaaa4b6727ce07a7e8b9e
Bruce Alderson <balderson@discoverysoftware.com>
2012-06-06 15:46:06 -0700

    Improved global PHP fail handler, returning JSON instead of plaintext. Cleaned up and documented delegation code (as part of general review/doc cleanup).

--- lib/presto.php
+++ lib/presto.php
@@ -1,106 +1,109 @@
-<?php 
-/**
-	Presto micro web services framework
-	(pico REST to $x)
-	
-	Prototype 2
-		
-		service
-			request		method	path	options
-*/
+<?php include_once('_config.php');
 
-include_once('_config.php');
 include_once(PRESTO_BASE.'/_helpers.php');
 include_once(PRESTO_BASE.'/api.php');
 
+/** Presto micro web services framework
+
+*/
 class Presto extends REST {
 	public $call;
-	public static $v;
+	public static $v; // version
 	
-	public function __construct($v = '') { 	
+	/** Initialize with the request, and start delegation */
+	public function __construct($v = '' /* version */) {
+
 		$this->_base = $_SERVER['DOCUMENT_ROOT'];
 		set_error_handler(array($this, 'fail'));
+		
 		self::$v = $v;
 		self::$req = new request();
 
 		try {
-
-			$this->filter();
-			$this->dispatch();
-			
+			$this->filter(); // request filtering
+			$this->dispatch(); // dispatch to loaded class->member based on $req	
 		} catch (Exception $e) {
-			throw $e;
+			throw $e; // errors are handled by delegator
 		}
 	}
 	
-	public function __toString() { return print_r($this, true); }
-	
+	/** Apply request filters */
 	private function filter() {	
-		// TODO
 	}
 	
 	/* Dispatch requests to classes and class methods */
 	private function dispatch() {
 		
-		$obj = self::$req->uri->component('error');
-		$o = new $obj();
-
-		$action = self::$req->action;
-		$thing = self::$req->uri->thing();
-
-		if (!$o->validConcept($thing))
-			$thing = ''; // don't use param as concept
-
-		$method = (strlen($thing)) ? "{$action}_{$thing}" : $action;
-
-		$this->call = (object) array(
-			'class' => $obj,
-			'method' => $method,
-			'res' => self::$req->uri->type(), 
-			'params' => self::$req->uri->parameters,
-			'exists' => false);			
-
-		self::$resp = new response($this->call, self::$v);
-
-		if ($obj == 'error')
-			throw new Exception('Root access not allowed', 403);
-		
-		if (!method_exists($obj, $method))
-			throw new Exception("Can't find $obj->$method()", 404);	
-		
-		$this->call->exists = true; 
-		
-		self::_trace("Dispatching to $obj :: $method");
-		
 		try {
+	
+			$obj = self::$req->uri->component('error' /* default to an error route */);
+			$o = new $obj();	
+			$action = self::$req->action;	// determines the request action (method)
+			$thing = self::$req->uri->thing(); // determine the thing (resource)
+	
+			if (!$o->validConcept($thing))
+				$thing = ''; // no thing (resource) available, assume root action
+	
+			// build the call pseudo object
+			$method = (strlen($thing)) ? "{$action}_{$thing}" : $action;	
+			$this->call = (object) array(
+				'class' 	=> $obj,
+				'method' 	=> $method,
+				'res' 		=> self::$req->uri->type(), 
+				'params' 	=> self::$req->uri->parameters,
+				'exists' 	=> false);			
+	
+			// build the response object
+			self::$resp = new response($this->call, self::$v);
+	
+			// verify the request
+			
+			if ($obj == 'error') // disallow root component access
+				throw new Exception('Root access not allowed', 403);
+			
+			if (!method_exists($obj, $method)) // check that the resource is valid
+				throw new Exception("Can't find $obj->$method()", 404);	
+
+			$this->call->exists = true; 
+			
+			self::_trace("Dispatching to $obj :: $method");		
 		
+			// delegate
 			$this->call->data = $o->$method( $this->call, self::$req->body() );
-			
+		
+			// 
+			if (is_object($this->call->data) || is_array($this->call->data))
+				return self::$resp->ok($this->call->data, self::$req);
+			else
+				return $this->call->data;
+
 		} catch (Exception $e) {			
-			
 			self::$resp->hdr($e->getCode());
 			throw $e;
 		}
-	
-		if (is_object($this->call->data) || is_array($this->call->data))
-			return self::$resp->ok($this->call->data, self::$req);
-		else
-			print $this->call->data;
-			
-		return true;
 	}
 
-	
+	/** Presto failures (including many PHP failures) */
 	static public function fail($n, $text, $file, $line, $ctx) {
-		self::$resp = new response();
-		$codes = array(2 => '404');	
+		$call = (object) array('res' => 'json');
+		self::$resp = new response($call);
 
+		$details = array(
+			'code' => $n,
+			'text' => $text, 
+			'file' => $file,
+			'line' => $line,
+			'context' => $ctx
+		);
+
+		$codes = array(2 => '404');
 		self::$resp->hdr(coalesce(@$codes[$n], '500'));
-
-		$extra = !empty($ctx) ? "\n\nParameters:\n" . print_r($ctx, true) : '';
-		die("#$n\n$text\n$file:$line$extra\n");
-	
+		
+		print json_encode($details);
 	}
+
+	/** Debugging dump of Presto delegator */
+	public function __toString() { return print_r($this, true); }
 }
 
 /** REST base class
