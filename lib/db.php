25f4143b144898331ecd2ea239fec3e5cc4e9e51
Adam Patterson <apatterson@discoverysoftware.com>
2012-06-08 14:23:35 -0700

    Error checking has been moved into the db class. All functions that touch the db now do an explicit check for errors on the last operation. User defined errors are thrown with a 413, other errors are thrown with a 500.

--- lib/db.php
+++ lib/db.php
@@ -11,19 +11,23 @@
 class db extends PDO {
 
 	private $statement; 
+	const USR_DEF_DB_ERR 	= '45000'; 
+	const DB_NO_ERR 		= '00000';
 	
 	/* Returns an array of classes based on the given SQL and bound parameters (see PDO docs for details) */
 	function select($sql, $bound_parameters = array()) {		
 		$this->statement = $this->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 		$this->statement->execute($bound_parameters);
-		return $this->statement->fetchAll(PDO::FETCH_CLASS);	
+		$resultset = $this->statement->fetchAll(PDO::FETCH_CLASS);
+		$this->errors();
+		return $resultset;
 	} 
 	
 	/* Wrapper for updates.
 		$bound_parameters is an array of arrays with the 'value' member as the value to bind and the 'pdoType' as 
 		that parameter's PDO datatype.
 	 */
-	function query($sql, &$bound_parameters = array()) {return $this->update($sql, $bound_parameters);}
+	function query($sql, &$bound_parameters = array()) {$this->update($sql, $bound_parameters);}
 	function update($sql, &$bound_parameters = array()) {	
 		$this->statement = $this->prepare($sql);	
 		foreach ($bound_parameters as $key => &$param) {
@@ -31,23 +35,32 @@ class db extends PDO {
 			if (!$this->statement->bindValue($key, $v, $t))
 				throw new Exception("Unable to bind '$v' to named parameter ':$key'.", 413);
 		}
-
-		return $this->statement->execute();	
+		
+		$this->statement->execute();
+		$this->errors();	
 	}
 	
 	/* Provide a wrapper for inserts which is really just an alias for the update function. */
 	function insert($sql, &$bound_parameters = array()) {
-		return $this->update($sql, $bound_parameters);
-	}
-	
-	/* Return error info pertaining to the last operation. */
-	function errors() {
-		return $this->statement->errorInfo();
+		$this->update($sql, $bound_parameters);
 	}
 	
 	/* Return the number of rows affected by the last INSERT, DELETE, or UPDATE.  */
 	function affected_rows() {
 		return $this->statement->rowCount();
 	}
-
+	
+	/* Throw error info pertaining to the last operation. */
+	private function errors() {
+		$e = $this->statement->errorInfo();
+		if (empty($e[0]) || $e[0] === self::DB_NO_ERR) return;
+		if (!empty($e[0]) && $e[0] === self::USR_DEF_DB_ERR) {
+			$msg = !empty($e[2]) ? $e[2] : 'Application defined SQL error occurred.';
+			throw new Exception($msg, 412);
+		}
+		else if (!empty($e[0]) && !empty($e[2])) {
+			throw new Exception($e[2], 500);
+		}
+		else throw new Exception('Update failed for unknown reason.', 500);
+	}
 }
