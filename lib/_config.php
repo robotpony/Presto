c179531e9b284b5f5231bd6bdaf9c36cc6800c71
Bruce Alderson <mx@warpedvisions.org>
2012-08-31 12:48:59 -0700

    Extended config to include commonly used helper DEFINEs (and cleaned up docs).

--- lib/_config.php
+++ lib/_config.php
@@ -1,4 +1,6 @@
-<?php // Presto global config and constants
+<?php 
+
+// Presto global config and constants
 
 define('PRESTO', 'presto.php');
 define('PRESTO_DEBUG', 0);
@@ -7,14 +9,20 @@ define('DEFAULT_RES_TYPE', '.html');
 define('PRESTO_BASE', dirname(__FILE__));
 define('API_BASE', realpath($_SERVER['DOCUMENT_ROOT']));
 
-$builtIns = array('API', 'request', 'session');
+// Helpful shortcut constants
 
+define('HOST', isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost');
+define('PROTOCOL', isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443 ? 'https://': 'http://'); // TODO: handle common load balancers?
+define('BASE_URL', 	PROTOCOL.HOST);
+
+// Set up paths for simple auto class loading
 set_include_path(get_include_path()
   . PATH_SEPARATOR . PRESTO_BASE
   . PATH_SEPARATOR . API_BASE
   . PATH_SEPARATOR . API_BASE . '/lib/'
   . PATH_SEPARATOR . API_BASE . '/lib/extras/');	
 
+// Set up a base exception for PHP errors (redirects most PHP errors as Exeptions for more consistent handling from APIs)
 class PrestoException extends Exception { 
 	public static function errorHandlerCallback($code, $string, $file, $line, $context) {
 	
@@ -30,8 +38,10 @@ class PrestoException extends Exception {
 	}
 } 
 
+// Set up PHP error handling (note these settings are overriden by explicit PHP ini settigns, we should address this)
+
 assert_options(ASSERT_WARNING, 0);
-ini_set('html_errors',false);
+ini_set('html_errors', false);
 error_reporting(E_ALL);
 set_error_handler(array("PrestoException", "errorHandlerCallback"), E_ALL);
 
