ba0e6a7f2893ce4e0084b927e4101e9fd61fa4e0
Bruce Alderson <mx@warpedvisions.org>
2012-09-09 22:01:00 -0700

    Rearanged nav menu layouts.

--- docs/nav.php
+++ docs/nav.php
@@ -1,8 +1,8 @@
 <nav>
 	<a href="https://github.com/robotpony/Presto/zipball/master">Download beta .zip</a>
-	<a href="https://github.com/robotpony/Presto/issues">Support and issues</a>
+	<a href="https://github.com/robotpony/Presto/issues">Issue tracker</a>
 
+<!--	<a href="/tutorial.html">Interactive Tutorial</a> -->
+	<a href="/setup-tests">Install checklist</a>
 	<a href="/docs/index.html">Developer docs</a>
-
-	<a href="/setup-tests/">Install checklist</a>
 </nav>
