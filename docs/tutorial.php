b6d933c36a4d34a4afde30d7fac3b410b918ac39
Bruce Alderson <mx@warpedvisions.org>
2012-09-09 21:59:58 -0700

    Added future tutorial page

---
+++ docs/tutorial.php
@@ -0,0 +1,16 @@
+
+<var class="server">http://presto.napkinware.com/docs/example.json</var>
+
+<div id="request">
+	<pre class="header">
+	</pre>
+	<pre class="body">
+	</pre>
+</div>
+
+<div id="response">
+	<pre class="header">
+	</pre>
+	<pre class="body">
+	</pre>
+</div>
