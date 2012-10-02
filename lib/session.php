be6f163e7a78580ce179b77fab97b7455688df47
Bruce Alderson <balderson@discoverysoftware.com>
2012-10-02 13:07:59 -0700

    Fixed up session to return exceptions (versus true/false) as from login service.

--- lib/session.php
+++ lib/session.php
@@ -7,16 +7,16 @@ class session {
 
 	private $t;
 	private $cfg;
-	
-	/* Set up session 
-	
+
+	/* Set up session
+
 	## Parameters
-	
+
 		session name
-		extra settings		
+		extra settings
 	*/
 	public function __construct($serviceID, $settings = null) {
-	
+
 		$this->cfg = array(
 			'cookie_name' 		=> SERVICE_COOKIE,
 			'service' 			=> $serviceID,
@@ -30,41 +30,40 @@ class session {
 		if ($settings) $this->cfg = (object) array_merge($settings, $this->cfg);
 		else $this->cfg = (object) $this->cfg;
 
-		if (empty($this->cfg->cookie_name) || !isset($this->cfg->domain) || 
+		if (empty($this->cfg->cookie_name) || !isset($this->cfg->domain) ||
 			!isset($this->cfg->secure))
 			throw new Exception('Missing Session class configuration.', 500);
 	}
-	
+
 
 	/* Is the request authorized? */
 	public function is_authorized() {
-	
+
 		try {
-		
+
 			if ($this->supports_api_keying())
 				if ($this->valid_key()) return true;
-				
+
 			if (!isset($_COOKIE[$this->cfg->cookie_name]))
-				return false; // not signed in
-				
+				throw new Exception('Not authorized (no session cookie).', 401); // not signed in
+
 			$this->t = new auth_token($_COOKIE[$this->cfg->cookie_name]);
-			
-			if (!$this->t->ok()) 
-				return $this->redirect('log-in.html?a=token-revoked&m=Invalid&20token'); // invalid auth
-			
+
+			if (!$this->t->ok())
+				throw new Exception('Invalid authorization token.', 412); // invalid auth
+
 		} catch (Exception $e) {
-			dump($e->getMessage());
-			return false;
+			throw new Exception('Authorization error.', 500, $e);
 		}
-		
+
 		return true; // signed in
 	}
-	
+
 	/* Is the client attempting to authorize? */
 	public function is_authorizing() {
 		return (array_key_exists('email', $_POST) && $_SERVER['REQUEST_METHOD'] == 'POST');
 	}
-	
+
 	/* Redirect the client (this seems out of place) -- >> RESPONSE  */
 	public function redirect($target) {
 		return header('Location: /'.$target);
@@ -77,31 +76,31 @@ class session {
 		if (is_array($p) && count($p)) {
 			$params = '?';
 			foreach ($p as $k => $v) $params .= "$k=$v&";
-		} elseif (is_string($p)) 
+		} elseif (is_string($p))
 			$params = '?'.$p;
-			
+
 		return $target.$params;
 	}
-	
+
 	/* Save the session */
 	public function save($t, $r = false) { // TODO domain, secure, http-only
 		$expiry = $r ? 36000 : 600;
 
-		if (empty($this->cfg->cookie_name) 
-			|| !setcookie($this->cfg->cookie_name, $t, time() + $expiry, '/', 
+		if (empty($this->cfg->cookie_name)
+			|| !setcookie($this->cfg->cookie_name, $t, time() + $expiry, '/',
 				$this->cfg->domain, $this->cfg->secure))
 			throw new Exception('Cookie monster sad :-(', 500);
 	}
 	/* Clear the session */
 	public function clear() {
-		setcookie($this->cfg->cookie_name, '', time() - 1, '/', 
-			$this->cfg->domain, $this->cfg->secure);
+		if (!setcookie($this->cfg->cookie_name, '', time() - 1, '/', $this->cfg->domain, $this->cfg->secure))
+			throw new Exception('Failed to remove cookie', 500);
 	}
-	
+
 	// get the current token
 	public function token() { return $this->t->ok() ? $this->t->encoded() : false; }
 	// get info about the current user
-	public function user() { return is_object($this->t) && $this->t->ok() ? 
+	public function user() { return is_object($this->t) && $this->t->ok() ?
 		$this->t->parts() : false; }
 	// can an API key work?
 	private function supports_api_keying() { return $this->cfg->api_key_header; }
@@ -111,6 +110,6 @@ class session {
 	private function valid_key() {
 		return ($this->supports_api_keying() && $_SERVER[$this->cfg->api_key_header] === $this->cfg->sso_key);
 	}
-	
+
 
 }
