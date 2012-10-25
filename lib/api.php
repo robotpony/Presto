874aa0fa0b0d461f40092668b59f9a76f731a550
Adam Patterson <apatterson@discoverysoftware.com>
2012-10-25 14:55:32 -0700

    Correct ordering of things being folded into defaults

--- lib/api.php
+++ lib/api.php
@@ -98,7 +98,7 @@ class API extends REST {
 	
 	/* Get a filtered variable (get filter_var_array + exceptions) */
 	static function filtered($thing, $rules, $defaults = null) {
-		if ($defaults) $thing = array_merge((array)$thing, $defaults);
+		if ($defaults) $thing = array_merge($defaults, (array)$thing);
 		$filtered = filter_var_array((array)$thing, $rules);
 
 		if ( $filtered === null )
