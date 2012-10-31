411f007357e193d6febae99a9898fe0c6a68a695
Bruce Alderson <mx@warpedvisions.org>
2012-10-31 15:54:46 -0700

    Extended views for better output/parameter checking.

--- lib/view.php
+++ lib/view.php
@@ -6,30 +6,40 @@ class View {
 	private $d;
 	private $v;
 	private $r;
+	private $f;
 	public static $root;
 	
-	function __construct($view = 'index', $data = null) {
-		if ($data !== null) $this->d = $data;
+	public function __construct($view = 'index', $data = null) {
 		
-		if (empty(self::$root)) self::$root = PRESTO_BASE . '../_views';
-		if (!is_dir(self::$root)) self::$root = realpath(__DIR__) . '/';	
-		$this->v = self::$root."$view.php";
+		// hook view parameters
 		
-		if (!is_file($this->v)) 
-			throw new Exception("View '$view' not found in '".self::$root."'.", 501);
-		
-		$this->display();
+		$this->d = array('dom' => $data); // namespaced into "dom"
+		$this->f = $view;
 	}
 	
-	function display() {
-		// TODO: hook presto constants in? other constants?
-		// NOTE: would be nice to hook in $app construct of some sort (or other globals)
-		
+	public function render() {		
 		try {
+		
+			// verify and load view
+			if (!is_dir(self::$root)) self::$root = realpath(__DIR__) . '/';	
+			if (empty(self::$root)) self::$root = PRESTO_BASE . '../_views';
+			$this->v = self::$root."/{$this->f}.php";
+		
+			if (!is_file($this->v)) 
+				throw new Exception("View ({$this->f}) not found in '".self::$root."'.", 501);
+		
+			// render view and return
 			extract($this->d);
-			require($this->v);
+			ob_start();
+			include($this->v);
+			$output = ob_get_contents();
+			ob_end_clean();			
+			return $output;
+			
+			// note that this could be faster (if called post-header creation in Presto)
+			
 		} catch (Exception $e) {
-			print "Failed to show view."; // should this rethrow?	
+			throw new Exception('Failed to render view.', 500, $e);
 		}
 	}
 }
