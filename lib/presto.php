c049d9ff35e8461977c3d418939e1dd6fc43dfc4
Bruce Alderson <balderson@discoverysoftware.com>
2013-02-18 11:08:13 -0800

    Base PrestoPHP documentation and code cleanup. Root autoloaders moved to helpers file.

--- lib/presto.php
+++ lib/presto.php
@@ -3,40 +3,13 @@
 include_once(PRESTO_BASE.'/_helpers.php');
 include_once(PRESTO_BASE.'/api.php');
 
-/**
- * Automatically load classes that aren't included.
- *
- * @param string $class (Required) The classname to load.
- * @return boolean Whether or not the file was successfully loaded.
- */
-function presto_autoloader($class) {
-	// First look in the base directory for the web app
-	$class_file = strtolower($class) . ".php";
-	if (file_exists($class_file)) {
-		require_once($class_file);
-		return true;
-	}
-	// Next look in the Presto library directory
-	$path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
-	$lib_file = $path . $class_file;
-	if (file_exists($lib_file)) {
-		require_once($lib_file);
-		return true;
-	}
-	// We can't find it so we let other autoloaders try
-	return false;
-}
-
-// Register the autoloader.
-spl_autoload_register('presto_autoloader');
-
-/** Presto micro web services framework
+/* Presto micro web services framework
 
 */
 class Presto extends REST {
 	public $call;
-	
-	/** Initialize with the request, and start delegation */
+
+	/* Initialize with the request, and start delegation */
 	public function __construct() {
 
 		$this->_base = $_SERVER['DOCUMENT_ROOT'];
@@ -45,17 +18,12 @@ class Presto extends REST {
 		self::$req = new request();
 
 		try {
-			$this->filter(); // request filtering
-			$this->dispatch(); // dispatch to loaded class->member based on $req	
+			$this->dispatch(); // dispatch to loaded class->member based on $req
 		} catch (Exception $e) {
 			throw $e; // errors are handled by delegator
 		}
 	}
-	
-	/** Apply request filters */
-	private function filter() {	
-	}
-	
+
 	private static function autoload_explicit($class) {
 		// First look in the base directory for the web app
 		$class_file = strtolower($class) . ".php";
@@ -64,76 +32,79 @@ class Presto extends REST {
 
 		throw new Exception("API `$class` not found.", 404);
 	}
-	
+
 	/* Dispatch requests to classes and class methods */
 	private function dispatch() {
-		
+
 		try {
-	
+
 			$obj = self::$req->uri->component('error' /* default to an error route */);
 			$action = self::$req->action;	// determines the request action (method)
 			$thing = self::$req->uri->thing(); // determine the thing (resource)
-				
-			// Create an an instance of the API subclass
-			//	(autoloads based on the path)
+
+			// Create an an instance of the API subclass (autoloaded)
+			
 			self::autoload_explicit($obj);
 			if (!class_exists($obj)) throw new Exception("API not found for $obj", 404);
-			$o = new $obj(); 
+			$o = new $obj();
 
-			// validate that the concept noun is valid
+			// Calidate that the concept (noun) is valid
+			
 			if (!$o->is_valid_concept($thing))
-				$thing = ''; // no thing (resource) available, assume root action
+				$thing = ''; // no concept available, assume root resource
 
-			// build the call pseudo object
-			$method = (strlen($thing)) ? "{$action}_{$thing}" : $action;	
+			// Build the call pseudo object
+
+			$method = (strlen($thing)) ? "{$action}_{$thing}" : $action;
 			$this->call = (object) array(
 				'class' 	=> $obj,
 				'method' 	=> $method,
-				'res' 		=> self::$req->uri->type(), 
+				'res' 		=> self::$req->uri->type(),
 				'params' 	=> self::$req->uri->parameters,
-				'options'   => self::$req->uri->options,
-				'exists' 	=> false);			
-	
-			// build the response object
-			self::$resp = new response($this->call);
-				
-			// verify the request
+				'options'	=> self::$req->uri->options,
+				'exists' 	=> false
+			);
+
+			// Start the response setup
 			
+			self::$resp = new response($this->call);
+
+			// Verify the request
+
 			if ($obj == 'error') // disallow root component access
 				throw new Exception('Root access not allowed', 403);
-			
-			if (!method_exists($obj, $method)) // check that the resource is valid
-				throw new Exception("Can't find $obj->$method()", 404);	
 
-			$this->call->exists = true; 
+			if (!method_exists($obj, $method)) // check that the resource is valid
+				throw new Exception("Can't find $obj->$method()", 404);
+
+			$this->call->exists = true;
+			self::_trace("Dispatching to $obj :: $method");
+
+			// Perform the actual sub delegation
 			
-			self::_trace("Dispatching to $obj :: $method");	
-		
-			// delegate
 			$o->attach( $this->call, self::$resp, self::$req );
 			$this->call->data = $o->$method( $this->call, self::$req->body() );
-		
-			// return the result to the client
-			$encode = (is_object($this->call->data) || is_array($this->call->data));
 
-			return self::$resp->ok( $this->call, $encode, $o->status(), $o->headers() );
+			// Produce a response for the client
 			
+			$encode = (is_object($this->call->data) || is_array($this->call->data));
+			return self::$resp->ok( $this->call, $encode, $o->status(), $o->headers() );
+
 		} catch (Exception $e) {
-			if (self::$resp === null)
-				self::$resp = new response();
-				
+			if (self::$resp === null) self::$resp = new response();
+
 			self::$resp->hdr($e->getCode());
 			throw $e;
 		}
 	}
 
-	/** Presto failures (including many PHP failures) */
+	/* Handle PrestoPHP failures (including all catchable PHP failures) */
 	static public function fail($n, $text, $file, $line, $ctx) {
 
 		// set up pseudo call and response
 		if (self::$resp === null)
 			self::$resp = new response($ctx);
-		
+
 		// generate useful HTTP status
 		switch ($n) {
 			case 2: $status = 400; break;
@@ -144,7 +115,7 @@ class Presto extends REST {
 		$details = (object) array(
 			'status' => $status,
 			'code' => $n,
-			'error' => $text, 
+			'error' => $text,
 			'file' => $file,
 			'line' => $line,
 			'ctx' => $ctx
@@ -161,7 +132,7 @@ class Presto extends REST {
 
 /** REST base class
 
-	Handy constants and base methods
+	Constants and base methods
 */
 class REST {
 
@@ -169,14 +140,14 @@ class REST {
 		'get', 'put', 'post', 'delete', 'options', 'head');
 	public static $TYPES = array(
 		'json', 'xml');
-		
+
 	public static $req;
-	public static $resp; 
-	public static $sess; 
-	
+	public static $resp;
+	public static $sess;
+
 	public static function _trace() {
 		if (PRESTO_DEBUG == 0) return;
 		error_log("TRACE: ".implode("\n\t", func_get_args()));
 	}
-	
+
 }
