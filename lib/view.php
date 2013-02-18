9e3a52cceb1156d3600f53376fbfc50813b96336
Bruce Alderson <balderson@discoverysoftware.com>
2013-02-18 10:18:55 -0800

    Cleaned up documentation and calling path.

--- lib/view.php
+++ lib/view.php
@@ -1,6 +1,13 @@
 <?php
 
-/* A basic view helper
+/* A simple PrestoPHP view
+
+	Usage:
+	
+		print new View('login', array( 'user' => 'test-user' ) );
+	
+	Returns the rendered text of the view or throws a status code (as a standard PHP exception).
+
 */
 class View {
 	private $d;
@@ -8,36 +15,37 @@ class View {
 	private $r;
 	private $f;
 	public static $root;
-	
+
 	public function __construct($view = 'index', $data = null) {
-		
+
 		// hook view parameters
-		
+
 		$this->d = array('dom' => $data); // namespaced into "dom"
 		$this->f = $view;
+
+		return $this->render();
 	}
-	
-	public function render() {		
+
+	protected function render() {
 		try {
-		
 			// verify and load view
-			if (!is_dir(self::$root)) self::$root = realpath(__DIR__) . '/';	
+
+			if (!is_dir(self::$root)) self::$root = realpath(__DIR__) . '/';
 			if (empty(self::$root)) self::$root = PRESTO_BASE . '../_views';
 			$this->v = self::$root."/{$this->f}.php";
-		
-			if (!is_file($this->v)) 
+
+			if (!is_file($this->v))
 				throw new Exception("View ({$this->f}) not found in '".self::$root."'.", 501);
-		
+
 			// render view and return
+
 			extract($this->d);
 			ob_start();
 			include($this->v);
 			$output = ob_get_contents();
-			ob_end_clean();			
+			ob_end_clean();
 			return $output;
-			
-			// note that this could be faster (if called post-header creation in Presto)
-			
+
 		} catch (Exception $e) {
 			throw new Exception('Failed to render view.', 500, $e);
 		}
