c9fb4435a191f15b347d4d5b2f3e62a0f6eb5d14
Bruce Alderson <balderson@discoverysoftware.com>
2013-02-19 14:14:06 -0800

    Split out helpers.

--- lib/_tests/dispatching.sh
+++ lib/_tests/dispatching.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-OK='\033[38;1;32m[OK]\x1b[0m'
-F='\033[38;1;31m[FAIL]\x1b[0m'
-SKIP='\033[38;1;31m[SKIP]\x1b[0m'
 DEBUG=$1||0
-BASE_URL=presto.test
+DIR="${BASH_SOURCE%/*}"
 
-get() {
-	echo -n "GET $1"
-	response=`curl -s ${BASE_URL}/$1`
-	
-	case $? in
-		22 )
-			echo -e " #22 ${F}" ;;
-		* )
-			echo -e " #$? ${OK}"			
-	esac
-	
-	if [ ${DEBUG} ] ; then echo ${response} | python -mjson.tool; fi
-}
+. "${DIR}/utils.sh"
 
-get "setup-tests/info.json"
-get "setup-tests/info/utf8.json"
-get "transmogrify/system/httpd.json"
-get "transmogrify/system/httpd/headers.json"
+
+# get "setup-tests/info.json"
+get "setup-tests/info/header-test.json"
+# get "setup-tests/info/utf8.json"
+# get "transmogrify/system/httpd.json"
+# get "transmogrify/system/httpd/headers.json"
