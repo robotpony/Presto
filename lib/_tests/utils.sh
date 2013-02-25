0e2bcdd0453a036922906bb940f1f8278432adc6
Bruce Alderson <bruce@napkinware.com>
2013-02-24 23:59:31 -0800

    Added docs to testing script.

--- lib/_tests/utils.sh
+++ lib/_tests/utils.sh
@@ -12,6 +12,9 @@ section() {
     printf "\n\033[38;1;34m%s [ %s ] %s\x1b[0m \n\n" ${LINE:${#TEXT}} "$TEXT" ${LINE:${#TEXT}}
 }
 
+# make a curl request (helper)
+#
+# METHOD URL [TYPE] [EXTRA SETTINGS]
 curlr() {
     m=`echo $1 | tr '[a-z]' '[A-Z]'`
     x="-X ${m}"
@@ -24,7 +27,6 @@ curlr() {
     printf "\033[38;1;34m[%s]\x1b[0m %s " "$m" "$2"
 
     cmd="curl -X ${m} ${extra} -s ${BASE_URL}/$2"
-    echo $cmd
 	response=$($cmd)
 	
 	case $? in
@@ -46,18 +48,23 @@ curlr() {
     fi
 }
 
+# A get request
 get() { 
     curlr 'GET' $1 $2 $3 $4
 }
+# An options request
 options() {
     curlr 'OPTIONS' $1 $2 $3 $4
 }
+# A post
 post() {
     curlr 'POST' $1 $2 $3 $4
 }
+# A put
 put() {
     curlr 'PUT' $1 $2 $3 $4
 }
+# A delete
 delete() {
     curlr 'DELETE' $1 $2 $3 $4
 }
