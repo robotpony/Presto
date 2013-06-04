d2b62b2cb8c31bd912f88aeab5fbd3c4b8b43147
Bruce Alderson <balderson@discoverysoftware.com>
2013-06-04 10:50:52 -0700

    Updated version.

--- lib/_config.php
+++ lib/_config.php
@@ -3,7 +3,7 @@
 // Presto global config and constants
 
 define('PRESTO', 'presto.php');
-define('PRESTO_VERSION', 'presto-v1.10');
+define('PRESTO_VERSION', 'presto-v1.11');
 define('PRESTO_DEBUG', 0);
 define('PRESTO_TRACE', 1);
 define('PRESTO_TRACE_KEY', '_presto_trace');
