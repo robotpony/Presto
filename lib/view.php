e24f236656fe562eb5d977a44c8a7d6cc9e58a28
Bruce Alderson <balderson@discoverysoftware.com>
2013-08-06 10:34:01 -0700

    Whitespace cleanup.

--- lib/view.php
+++ lib/view.php
@@ -1,11 +1,14 @@
 <?php
 
-/* A simple PrestoPHP view
+/* A simple output view
+
+	Often used to generate special output, like HTML or other indirect object->output mappings. Don't use
+	this to generate JSON or XML (those are more easily generated with DOM->adapter patterns)
 
 	Usage:
-	
+
 		print new View('login', array( 'user' => 'test-user' ) );
-	
+
 	Returns the rendered text of the view or throws a status code (as a standard PHP exception).
 
 */
@@ -17,16 +20,16 @@ class View {
 
 	public static function htmlize($v, $d) {
 		$v = new View($v, $d);
-		return $v->render();	
+		return $v->render();
 	}
-	
+
 	public function __construct($view = 'index', $data = null) {
 
 		// hook view parameters
 
 		$this->d = array('dom' => $data); // namespaced into "dom"
 		$this->f = is_array($view) ? $view : array($view);
-		
+
 		return $this;
 	}
 
@@ -36,20 +39,20 @@ class View {
 			// render view and return
 			extract($this->d);
 			ob_start();
-		
+
 			if (empty($this->f))
 				throw new Exception('You did not specify any view paths.', 501);
-		
+
 			// verify and load multipart view
 			foreach ($this->f as $f) {
 				$this->v = $f;
-	
+
 				if (!stream_resolve_include_path($this->v))
 					throw new Exception("View {$this->v} ({$this->v}) not found in: ".get_include_path().".", 404);
-					
+
 				include($this->v);
 			}
-				
+
 			$output = ob_get_contents();
 			ob_end_clean();
 			return $output;
