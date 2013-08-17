5c5c074c82bce98a0d0ef3e84d79800631e850e8
Bruce Alderson <balderson@discoverysoftware.com>
2013-08-17 15:59:55 -0700

    Updated view basics.

--- lib/view.php
+++ lib/view.php
@@ -3,9 +3,9 @@
 /* A simple PrestoPHP view
 
 	Usage:
-	
+
 		print new View('login', array( 'user' => 'test-user' ) );
-	
+
 	Returns the rendered text of the view or throws a status code (as a standard PHP exception).
 
 */
@@ -15,41 +15,44 @@ class View {
 	private $r;
 	private $f;
 
-	public static function htmlize($v, $d) {
-		$v = new View($v, $d);
-		return $v->render();	
+	// Render a view
+	public static function render($v, $d, $p = '') {
+		$v = new View("$p$v", $d);
+		return $v->renderer();
 	}
-	
+
+	// Class setup
 	public function __construct($view = 'index', $data = null) {
 
 		// hook view parameters
 
 		$this->d = array('dom' => $data); // namespaced into "dom"
 		$this->f = is_array($view) ? $view : array($view);
-		
+
 		return $this;
 	}
 
-	public function render() {
+	// Do the rendering
+	private function renderer() {
 		try {
 
 			// render view and return
 			extract($this->d);
 			ob_start();
-		
+
 			if (empty($this->f))
 				throw new Exception('You did not specify any view paths.', 501);
-		
+
 			// verify and load multipart view
 			foreach ($this->f as $f) {
 				$this->v = $f;
-	
+
 				if (!stream_resolve_include_path($this->v))
 					throw new Exception("View {$this->v} ({$this->v}) not found in: ".get_include_path().".", 404);
-					
+
 				include($this->v);
 			}
-				
+
 			$output = ob_get_contents();
 			ob_end_clean();
 			return $output;
