43a98a84a724781562756a325b8f1916f1193fb4
Bruce Alderson <balderson@discoverysoftware.com>
2013-09-05 12:46:21 -0700

    Relaxed service exceptions for 200/300 class responses. Improved returned error message.

--- lib/service.php
+++ lib/service.php
@@ -224,10 +224,9 @@ class Service {
 		if ($this->result->data === false)
 			throw new Exception("HTTP service error, no data: {$this->call->method} {$this->call->uri}", $this->call->info->http_code);
 
-		if ($this->call->info->http_code != 200) {
-			$dump = ($this->options->debug) ? print_r($this->result, true) 
-				: print_r($this->result->data, true);
-			throw new Exception("HTTP service error\n{$this->call->method} {$this->call->uri}\n\n$dump\n" , $this->call->info->http_code);
+		if ($this->call->info->http_code >= 400) {
+			$dump = ($this->options->debug) ? json_encode($this->result)  : json_encode($this->result->data);
+			throw new Exception("HTTP service error in {$this->call->method} for {$this->call->uri} - $dump", $this->call->info->http_code);
 		}
 		
 		return $this->data();
