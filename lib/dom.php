25226e77cad254ba3fe5edd61e7c29445816f5ea
Bruce Alderson <bruce@napkinware.com>
2013-11-30 15:07:13 -0800

    Adds a DOM base.

---
+++ lib/dom.php
@@ -0,0 +1,71 @@
+<?php /* Presto.md - Copyright (C) 2013 Bruce Alderson */
+
+namespace napkinware\presto;
+
+/* A simple DOM base class
+	
+	Provides:
+	
+	* a safe get mechanism
+	* 
+	
+	TODO
+	
+	* Consider two classes (DOM, DOM + HTML)
+*/
+class dom {
+
+	public $title;
+
+	protected $d;
+
+	/* create a DOM instance 
+		
+		$p - optional parameters (associative)
+	*/	
+	public function __construct($p = null) {
+		if (!$p) return;
+
+		assert($p !== array_values($p) && !is_object($p), 
+			'Properties should be associative.');
+
+		$this->d = $p;
+	}	
+		
+	/* Dynamic get/set/isset/unset 
+		
+		Allows DOM elements to be referenced safely
+	*/
+	public function __get($k) {
+        if (!array_key_exists($k, $this->d)) {
+	        return '';
+        }
+                
+		return $this->d[$k];
+    }
+	public function __set($k, $v) {
+		$this->d[$k] = $v;
+    }
+        
+    public function __isset($k) {
+        return array_key_exists($k, $this->d);
+    }
+
+    public function __unset($k) {
+    	if (array_key_exists($k, $this->d))
+    		unset($this->d[$k]);
+    }    
+
+	/* Dump the class usefully as HTML */
+	public function __toString() {
+?>
+<pre class="debug">
+<?= __CLASS__; ?> :
+
+<?= print_r($this); ?>
+
+<?= print_r($this->d); ?>
+</pre>
+<?php		
+	}
+}
