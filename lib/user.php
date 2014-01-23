e136dc256ffaecf120ed5257c527ff309b1a7786
Adam Patterson <apatterson@discoverysoftware.com>
2014-01-23 15:41:24 -0800

    First pass of user data model

--- lib/user.php
+++ lib/user.php
@@ -7,8 +7,22 @@
 */
 class user {
 
-	public function __construct($userData) {
-		print_r($userData);
+	public $u;
+
+	public function __construct($userData) { $this->u = $userData; }
+	
+	public function __get($key) {
+		switch ($key) {
+			case 'Account' :
+				return !empty($this->u->enrolments->account) ? $this->u->enrolments->account : 0;
+			case 'AccountID' :
+				return !empty($this->u->enrolments->account->ID) ? $this->u->enrolments->account->ID : 0;
+			case 'AccountName' :
+				return !empty($this->u->enrolments->account->name) ? $this->u->enrolments->account->name : 0;
+
+			default:
+				return !empty($this->u->{$key}) ? $this->u->{$key} : '';
+		}
 	}
 }
 
